f38df0c84ff12d84b9dcea98cec9afed2ea738ed
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-13 23:21:29 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+APP_ID = os.environ.get("APP_ID", None)
+API_HASH = os.environ.get("API_HASH", None)
+BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
+OWNER_ID = os.environ.get("OWNER_ID", None) # Bot Sahibinin Telegram Kullanıcı Adı
